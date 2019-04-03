8440e43c40752d0e557171c55241c83db0168c63
Phelan <phelanmccall@yahoo.com>
2019-04-02 18:00:38 -0700

    appointment getting work

--- client/src/components/AdminControls.js
+++ client/src/components/AdminControls.js
@@ -2,24 +2,24 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class AdminContols extends Component {
-     timesArr = [
-         "09:00AM",
-         "09:30AM",
-         "10:00AM",
-         "10:30AM",
-         "11:00AM",
-         "11:30AM",
-         "12:00PM",
-         "12:30PM",
-         "01:00PM",
-         "01:30PM",
-         "02:00PM",
-         "02:30PM",
-         "03:00PM",
-         "03:30PM",
-         "04:00PM",
-         "04:30PM",
-     ];
+    timesArr = [
+        "09:00AM",
+        "09:30AM",
+        "10:00AM",
+        "10:30AM",
+        "11:00AM",
+        "11:30AM",
+        "12:00PM",
+        "12:30PM",
+        "01:00PM",
+        "01:30PM",
+        "02:00PM",
+        "02:30PM",
+        "03:00PM",
+        "03:30PM",
+        "04:00PM",
+        "04:30PM",
+    ];
 
     state = {
         message: "",
@@ -28,44 +28,45 @@ class AdminContols extends Component {
         booked: [],
         appointments: []
     }
-    componentDidMount() {   
-      
-        axios.get("/services").then((response)=>{
-            if(response.data.length){
+    componentDidMount() {
+
+        axios.get("/services").then((response) => {
+            if (response.data.length) {
                 this.setState({
                     services: response.data
                 })
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
-        axios.get("/appointments").then((response)=>{
-            if(response.data.length){
+        axios.get("/appointments").then((response) => {
+            if (response.data.length) {
                 this.setState({
                     appointments: response.data
                 })
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
     }
 
     getBookings = (e) => {
         e.preventDefault();
-        axios.get(`/bookings/${e.target.value}`).then((response)=>{
+        axios.get(`/bookings/${e.target.value}`).then((response) => {
             console.log(response.data);
             this.setState({
                 bookings: response.data
             })
-        }); 
-       
+        });
+
     }
 
-    getAppointments = (e) =>{
-        axios.get("/appointments").then((res)=>{
-            if(res.data.lenth){
+    getAppointments = (e) => {
+        axios.get("/appointments").then((res) => {
+            if (res.data.lenth && typeof res.data === "object") {
+                console.log(res.data);
                 this.setState({
-                    appointments: res.data
+                    appointments: [...res.data]
                 })
             }
         })
@@ -77,9 +78,9 @@ class AdminContols extends Component {
 
     getBooked = (e) => {
         e.preventDefault();
-        axios.get(`/bookings/${e.target.value}`).then((response) =>{
-            let booked = response.data.reduce((array, val, key)=>{
-                if(!val.booked){
+        axios.get(`/bookings/${e.target.value}`).then((response) => {
+            let booked = response.data.reduce((array, val, key) => {
+                if (!val.booked) {
                     array.push(val);
                 }
                 return array;
@@ -87,66 +88,66 @@ class AdminContols extends Component {
             this.setState({
                 booked: booked
             })
-            
+
         });
     }
 
-    confirmBookings = (e) =>{
+    confirmBookings = (e) => {
         e.preventDefault();
-        var times = Array.from(document.querySelectorAll('input[name=confirmBook]:checked') ,(val, key) => {
+        var times = Array.from(document.querySelectorAll('input[name=confirmBook]:checked'), (val, key) => {
             return val.value;
         });
         console.log(times);
-       if(times.length){
-        axios.put(`/bookings/${e.target.date.value}`,
-        {time: times}).then((res)=>{
-            
-            this.setState({
-                booked: [],
-                message: res.data
-            });
-           
-        })
-       }
-       e.target.reset();
+        if (times.length) {
+            axios.put(`/bookings/${e.target.date.value}`,
+                { time: times }).then((res) => {
+
+                    this.setState({
+                        booked: [],
+                        message: res.data
+                    });
+
+                })
+        }
+        e.target.reset();
     }
 
     addBooking = (e) => {
         e.preventDefault();
-        var times = Array.from(document.querySelectorAll('input[name=addBook]:checked') ,(val, key) => {
+        var times = Array.from(document.querySelectorAll('input[name=addBook]:checked'), (val, key) => {
             return val.value;
         });
         console.log(times);
-       if(times.length){
-        axios.post(`/bookings`, {
-            date: e.target.date.value,
-            time: times
-        }).then((response)=>{
-            this.setState({
-                bookings: [],
-                message: response.data
-            })
-            e.target.reset();
-        }); 
-       }
+        if (times.length) {
+            axios.post(`/bookings`, {
+                date: e.target.date.value,
+                time: times
+            }).then((response) => {
+                this.setState({
+                    bookings: [],
+                    message: response.data
+                })
+                e.target.reset();
+            });
+        }
     }
 
-    deleteBooking = (e) =>{
+    deleteBooking = (e) => {
         e.preventDefault();
-        var times = Array.from(document.querySelectorAll('input[name=deleteBook]:checked') ,(val, key) => {
+        var times = Array.from(document.querySelectorAll('input[name=deleteBook]:checked'), (val, key) => {
             return val.value;
         });
-        if(times.length){
+        if (times.length) {
             axios.put(`/bookings`, {
                 date: e.target.date.value,
                 time: times
-            }).then((res)=>{
+            }).then((res) => {
                 this.setState({
                     bookings: [],
                     message: res.data
                 })
             });
-            
+
             e.target.reset();
         }
 
@@ -166,95 +167,95 @@ class AdminContols extends Component {
 
     }
 
-    deleteService = (e) =>{
+    deleteService = (e) => {
         e.preventDefault();
-        axios.delete(`/services/${e.target.service.value}`).then((res)=>{
+        axios.delete(`/services/${e.target.service.value}`).then((res) => {
             console.log(res.data);
         })
     }
-    updateService = (e) =>{
+    updateService = (e) => {
         e.preventDefault();
-        let { service, price} = e.target;
+        let { service, price } = e.target;
 
-        axios.put("/services", {service: service.value, price: price.value})
+        axios.put("/services", { service: service.value, price: price.value })
     }
     render() {
         return (
             <div>
                 <span>{this.state.message}</span>
                 <div id="adminControl">
-                    <form class="service" onSubmit={this.addService}>
-                        <label>Add Service</label><br/>
+                    <form className="service" onSubmit={this.addService}>
+                        <label>Add Service</label><br />
                         <label>Service</label>
                         <input name="service" required></input>
                         <label>Price</label>
                         <input name="price" required></input>
                         <input name="submit" type="submit" value="Submit" />
                     </form>
-                    <form class="service" onSubmit={this.updateService}>
-                        <label>Update Service</label><br/>
-                        <label>Service</label><br/>
+                    <form className="service" onSubmit={this.updateService}>
+                        <label>Update Service</label><br />
+                        <label>Service</label><br />
                         <select name="service" required>
                             {
-                                this.state.services.map((value, key)=>{
+                                this.state.services.map((value, key) => {
                                     return <option key={key}>
                                         {value.service}
                                     </option>
                                 })
                             }
-                        </select><br/>
+                        </select><br />
                         <label>Price</label>
-                        <input name="price" required></input><br/>
+                        <input name="price" required></input><br />
                         <input name="submit" type="submit" value="Update" />
                     </form>
-                    <form class="service" onSubmit={this.deleteService}>
-                        <label>Delete Service</label><br/>
-                        <label>Service</label><br/>
+                    <form className="service" onSubmit={this.deleteService}>
+                        <label>Delete Service</label><br />
+                        <label>Service</label><br />
                         <select name="service" required>
                             {
-                                this.state.services.map((value, key)=>{
+                                this.state.services.map((value, key) => {
                                     return <option key={key}>
                                         {value.service}
                                     </option>
                                 })
                             }
-                        </select><br/>
+                        </select><br />
                         <input name="submit" type="submit" value="Delete" />
                     </form>
-                    <form class="booking" onSubmit={this.addBooking}>
-                        <label>Add a Booking</label><br/>
+                    <form className="booking" onSubmit={this.addBooking}>
+                        <label>Add a Booking</label><br />
                         <label htmlFor="date">Date: </label>
                         <input type="date" name="date" min={new Date().toISOString().split("T")[0]} required /><br />
-                        <label>Check All</label><input type="checkbox" onClick={function(e){
+                        <label>Check All</label><input type="checkbox" onClick={function (e) {
                             var boxes = document.querySelectorAll("input[name=addBook");
-                            for(let i = 0; i < boxes.length; i++){
+                            for (let i = 0; i < boxes.length; i++) {
                                 boxes[i].checked = e.target.checked
                             }
-                        }} defaultChecked></input><br/>
+                        }} defaultChecked></input><br />
                         {
                             this.timesArr.map((val, key) => {
                                 return <span key={key}><label>{val}</label><input type="checkbox" name="addBook" value={val} defaultChecked></input></span>
                             })
                         }
-                        <br/>
+                        <br />
                         <input type="submit" name="submit" value="Add Booking(s)"></input>
                     </form>
-                    <form class="booking" onSubmit={this.deleteBooking}>
-                        <label>Delete a Booking</label><br/>
+                    <form className="booking" onSubmit={this.deleteBooking}>
+                        <label>Delete a Booking</label><br />
                         <label htmlFor="date">Date: </label>
                         <input type="date" name="date" onChange={this.getBookings} required /><br />
-                        <label>Check All</label><input type="checkbox" onClick={function(e){
+                        <label>Check All</label><input type="checkbox" onClick={function (e) {
                             var boxes = document.querySelectorAll("input[name=deleteBook");
-                            for(let i = 0; i < boxes.length; i++){
+                            for (let i = 0; i < boxes.length; i++) {
                                 boxes[i].checked = e.target.checked
                             }
-                        }} defaultChecked></input><br/>
+                        }} defaultChecked></input><br />
                         {
                             this.state.bookings.map((val, key) => {
                                 return <span key={key}><label>{val.time}</label><input type="checkbox" name="deleteBook" value={val.time} defaultChecked></input></span>
                             })
                         }
-                        <br/>
+                        <br />
                         <input type="submit" name="submit" value="Delete Booking(s)"></input>
                     </form>
                     {/* <form onSubmit={this.confirmBookings}>
@@ -281,40 +282,42 @@ class AdminContols extends Component {
                     </form> */}
                     <table>
                         <thead>
-                            <th>
-                                Name:
+                            <tr>
+                                <th>
+                                    Name:
                             </th>
-                            <th>
-                                Phone:
+                                <th>
+                                    Phone:
                             </th>
-                            <th>
-                                Date:
+                                <th>
+                                    Date:
                             </th>
-                            <th>
-                                Time:
+                                <th>
+                                    Time:
                             </th>
-                            <th>
-                                Confirmed:
+                                <th>
+                                    Confirmed:
                             </th>
+                            </tr>
                         </thead>
                         <tbody>
-                        {
-                            this.state.appointments.map((val, key)=>{
-                                return <tr>
-                                    <td>{val.name}</td>
-                                    <td>{val.phone}</td>
-                                    <td>{val.date}</td>
-                                    <td>{val.time}</td>
-                                    <td>false</td>
-                                </tr>
-                            })
-                        }
+                            {
+                                this.state.appointments.map((val, key) => {
+                                    return <tr>
+                                        <td>{val.name}</td>
+                                        <td>{val.phone}</td>
+                                        <td>{val.date}</td>
+                                        <td>{val.time}</td>
+                                        <td>false</td>
+                                    </tr>
+                                })
+                            }
                         </tbody>
                     </table>
+                </div>
+
             </div>
-   
-            </div>
-             );
+        );
     }
 }
 
